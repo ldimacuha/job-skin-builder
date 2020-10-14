dd81f8d2de16a221c9f2372b3cf075d084d54fcc
Leo Dimacuha <leo.dimacuha@gmail.com>
2020-10-14 12:41:52 +1000

    v1 base code tweaks

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,7 +5,7 @@ import { Navbar } from 'react-bootstrap';
 const Header = props => {
   return (
     <Navbar variant="dark" bg="dark" expand="lg">
-      <Navbar.Brand href="#home">{props.brand} <small className="align-top">beta</small></Navbar.Brand>
+      <Navbar.Brand href="#home">{props.brand}</Navbar.Brand>
       {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
